4d11f5280c426b52b195674872f8b98faf297b7c
Ian Zhang <benchi2@pdx.edu>
2023-11-22 15:28:52 -0600

    added product controller and configed redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,6 +8,8 @@ import {
   createBrowserRouter
 } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Provider } from 'react-redux';
+import store from './store';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -27,7 +29,9 @@ const router = createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
 
