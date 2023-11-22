88e4ec0605f6fb02c14f6755f767036a197fed5f
Ian Zhang <benchi2@pdx.edu>
2023-11-21 20:18:31 -0600

    created header component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,16 @@
+import Header from "./components/Header"
+import { Container } from "react-bootstrap"
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to MyShop</h1>
+        </Container>
+      </main>
+    </>
   )
 }
 
