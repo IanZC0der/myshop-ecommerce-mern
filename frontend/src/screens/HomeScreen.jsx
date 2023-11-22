d3412df9de9306ec241526d4c565b0a74b12ae1a
Ian Zhang <benchi2@pdx.edu>
2023-11-22 16:40:54 -0600

    added loader amd messager

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,11 +1,13 @@
 import {Row, Col} from 'react-bootstrap'
 import Product from '../components/Product'
 import { useGetProductsQuery } from '../slices/productsApiSlice'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
 const HomeScreen = () => {
   const {data: products, error, isLoading} = useGetProductsQuery()
   return (
     <>
-        {isLoading ? (<h2>Loading...</h2>) : error ? (<div>{error?.data?.message || error.error}</div>) : (
+        {isLoading ? (<Loader/>) : error ? (<Message variant="danger">{error?.data?.message || error.error}</Message>) : (
           <>
             <h1>Latest Products</h1>
             <Row>
--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -3,7 +3,8 @@ import { useParams } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
 import Rating from '../components/Rating'
 import { useGetProductDetailsQuery } from '../slices/productsApiSlice'
-
+import Loader from '../components/Loader'
+import Message from '../components/Message'
 const ProductScreen = () => {
     const { id: productId } = useParams()
     const { data: product, error, isLoading } = useGetProductDetailsQuery(productId)
@@ -11,7 +12,7 @@ const ProductScreen = () => {
     return (
         <>
             <Link className='btn btn-light my-3' to='/'>Go Back</Link>
-            {isLoading ? (<h2>Loading...</h2>) : error ? (<div>{error?.data?.message || error.error}</div>) : (
+            {isLoading ? (<Loader/>) : error ? (<Message variant="danger">{error?.data?.message || error.error}</Message>) : (
             <Row>
                 <Col md={5}>
                     <Image src={product.image} alt={product.name} fluid/>
