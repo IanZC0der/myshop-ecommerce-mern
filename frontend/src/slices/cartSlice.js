fdd0f35ff4a5761b236e3d045381fb62b814b605
Ian Zhang <benchi2@pdx.edu>
2023-11-22 17:41:35 -0600

    created cart slice

---
+++ frontend/src/slices/cartSlice.js
@@ -0,0 +1,39 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = localStorage.getItem("cart") ? JSON.parse(localStorage.getItem("cart")) : { cartItems: [] }
+
+const addDecimals = (num) => {
+    return (Math.round(num * 100) / 100).toFixed(2)
+}
+const cartSlice = createSlice({
+    name: 'cart',
+    initialState,
+    reducers: {
+        addToCart(state, action) {
+            const item = action.payload
+            const itemExists = state.cartItems.find(i => i._id === item._id)
+            if (itemExists) {
+                state.cartItems = state.cartItems.map(i => i._id === itemExists._id ? item : i)
+            } else {
+                state.cartItems = [...state.cartItems, item]
+            }
+
+            //calculate total price
+            state.itemsPrice = addDecimals(state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0))
+            //calculate shipping price
+            state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 20)
+
+            //calculate tax price
+            state.taxPrice = addDecimals(Number((0.15 * state.itemsPrice).toFixed(2)))
+
+            //calculate total price
+            state.totalPrice = addDecimals((Number(state.itemsPrice) + Number(state.shippingPrice) + Number(state.taxPrice)).toFixed(2))
+
+            localStorage.setItem("cart", JSON.stringify(state))
+        }
+    }
+})
+
+export default cartSlice.reducer
+
+export const { addToCart } = cartSlice.actions
