6049f5921c5fe31ca2d703cb3e019517e357561d
Ian Zhang <benchi2@pdx.edu>
2023-11-22 14:15:32 -0600

    created product routes

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import express from 'express'
-import products from './data/products.js'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
+import productRoutes from './routes/productRoutes.js'
+
 dotenv.config()
 const port=process.env.PORT || 5000
 
@@ -12,14 +13,10 @@ const app=express()
 app.get('/',(req,res)=>{
     res.send('Server is ready')
 })
-app.get('/api/products',(req,res)=>{
-    res.json(products)
-})
 
-app.get('/api/products/:id',(req,res)=>{
-    const product=products.find((p)=>p._id===req.params.id)
-    res.json(product)
-})
+app.use('/api/products',productRoutes)
+
+
 
 app.listen(port,()=>{
     console.log(`Server at http://localhost:${port}`)
