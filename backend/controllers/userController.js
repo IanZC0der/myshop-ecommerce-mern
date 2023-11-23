92147146fa790c8fdb3fa6bdeaa5f88fbdf4ad19
Ian Zhang <benchi2@pdx.edu>
2023-11-23 11:45:59 -0600

    created authMiddleware and generateToken util

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,7 @@
 import asyncHandler from '../middleware/asyncHandler.js'
 
 import User from '../models/userModel.js'
+import generateToken from '../utils/generateToken.js'
 
 /**
  * @desc    Auth user
@@ -14,6 +15,7 @@ const authUser = asyncHandler(async (req, res) => {
     const user = await User.findOne({ email })
 
     if (user && (await user.matchPassword(password))) {
+        generateToken(res, user._id)
         res.json({
             _id: user._id,
             name: user.name,
