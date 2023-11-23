52cd9cbb0919eb4bd5720fd4f370c76e6277b050
Ian Zhang <benchi2@pdx.edu>
2023-11-23 11:31:16 -0600

    updated authUser

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
 import {notFound,errorHandler} from './middleware/errorMiddleware.js'
+import userRoutes from './routes/userRoutes.js'
 
 dotenv.config()
 const port=process.env.PORT || 5000
@@ -10,12 +11,16 @@ const port=process.env.PORT || 5000
 connectDB()
 
 const app=express()
+//body parser
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
 app.get('/',(req,res)=>{
     res.send('Server is ready')
 })
 
 app.use('/api/products',productRoutes)
+app.use('/api/users',userRoutes)
 
 app.use(notFound)
 app.use(errorHandler)
