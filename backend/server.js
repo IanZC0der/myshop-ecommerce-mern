50ebda67253b64f6ec713a6774bb212dc33b505e
Ian Zhang <benchi2@pdx.edu>
2023-11-24 15:42:39 -0600

    added pagination limit

--- backend/server.js
+++ backend/server.js
@@ -21,9 +21,9 @@ app.use(express.urlencoded({extended:true}))
 
 app.use(cookieParser())
 
-app.get('/',(req,res)=>{
-    res.send('Server is ready')
-})
+// app.get('/',(req,res)=>{
+//     res.send('Server is ready')
+// })
 app.get('/api/config/paypal',(req,res)=>{
     res.send( {cliendId: process.env.PAYPAL_CLIENT_ID})
 })
@@ -35,6 +35,20 @@ app.use('/api/upload',uploadRoutes)
 
 const __dirname=path.resolve()
 app.use('/uploads',express.static(path.join(__dirname,'/uploads')))
+
+if(process.env.NODE_ENV==='production'){
+
+    app.use(express.static(path.join(__dirname,'/frontend/build')))
+
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'frontend','build','index.html'))
+    }
+    )
+} else {
+    app.get('/',(req,res)=>{
+        res.send('API is running')
+    })
+}
 
 
 app.use(notFound)
